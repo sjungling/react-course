3dee912d960b3b3fc446c184475ffe4402c7f5af
Scott Jungling <scott.jungling@build.com>
2016-03-04 16:13:14 -0800

    github battle, stateless functional components

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,19 @@
 var React = require('react');
+var transparentBg = require('../styles/index').transparentBg;
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+
 
 var Home = React.createClass({
   render: function() {
     return(
-      <div> Hello from home</div>
+      <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+        <h1>GitHub Battle</h1>
+        <p className='lead'>Fancy motto</p>
+        <Link to="/playerOne">
+          <button type="button" className="btn btn-lg btn-success">Get Started</button>
+        </Link>
+      </div>
     )
   }
 });
--- app/config/routes.js
+++ app/config/routes.js
@@ -3,14 +3,17 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
-var hashHistroy = ReactRouter.hashHistroy;
+var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var PromptContainer = require('../containers/PromptContainer');
 
 var routes = (
-  <Router history={hashHistroy}>
+  <Router history={hashHistory}>
     <Route path='/' component={Main}>
-      <IndexRoute component={Home} />
+      <IndexRoute  header='Player One' component={Home} />
+      <Route path='playerOne' header='Player One' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
     </Route>
   </Router>
 );
